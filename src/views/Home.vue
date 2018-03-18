8723be1b6f7759b7a162d6901cf43f553afae5d8
smilee <ideas007@naver.com>
2018-03-18 20:10:16 +0900

    Set up pages
    To start coding away

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,9 @@
 <template>
-  <div class="home">
-    <img src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <h1>The Home Component</h1>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
-
 export default {
   name: 'home',
-  components: {
-    HelloWorld,
-  },
 };
 </script>
